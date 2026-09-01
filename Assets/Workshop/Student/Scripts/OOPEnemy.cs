620211950c9a0b7037b0ac7818f67b67908c6438
EmiyaHana <warm310149@gmail.com>
2026-09-01 11:11:39 +0700

    SolutionPart1

--- Assets/Workshop/Student/Scripts/OOPEnemy.cs
+++ Assets/Workshop/Student/Scripts/OOPEnemy.cs
@@ -2,7 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OOPEnemy
+public class OOPEnemy : Character
 {
+    public void Start()
+    {
 
+    }
+
+    public 
 }
